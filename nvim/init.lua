d68e31313a57b1fd2388175fdbb923ea894c46b8
haku <haku1206@outlook.com>
2024-05-28 15:52:17 +0800

    nvim setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,8 +14,40 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-
-	{ "typicode/bg.nvim", lazy = false , cond = jit.os ~= 'Windows'},
-
+    {
+        "typicode/bg.nvim",
+        lazy = false,
+        -- not work on windows, so load on condiction
+        cond = jit.os ~= 'Windows'
+    },
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000, -- make sure to load this before all the other start plugins
+        -- Optional; default configuration will be used if setup isn't called.
+        config = function()
+          require("everforest").setup({
+            -- Your config here
+            -- check using ':h everforest'
+            background = 'soft',
+            disable_italic_comments = true,
+            sign_column_background = 'low',
+            diagnostic_virtual_text = "coloured",
+          })
+        end
+    }
 })
 
+
+vim.opt.background = 'light'
+vim.cmd.colorscheme('everforest')
+
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.opt.wrap = true
+vim.opt.breakindent = true
+indent_value = 4
+vim.opt.tabstop = indent_value
+vim.opt.shiftwidth = indent_value
+vim.opt.expandtab = true
