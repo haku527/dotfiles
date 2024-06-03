5399b92d030b18ce7a4a6239f0d3b08f4a75c7b7
haku <haku1206@outlook.com>
2024-06-04 06:35:00 +0800

    update

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,1 +1,14 @@
+Import-Module Terminal-Icons
+Set-TerminalIconsTheme -DisableColorTheme
+
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+$env:FZF_DEFAULT_OPTS='--color=bg+:#D9D9D9,bg:#E1E1E1,border:#C8C8C8,spinner:#719899,hl:#719872,fg:#616161,header:#719872,info:#727100,pointer:#E12672,marker:#E17899,fg+:#616161,preview-bg:#D9D9D9,prompt:#0099BD,hl+:#719899'
+
 Invoke-Expression (&starship init powershell)
