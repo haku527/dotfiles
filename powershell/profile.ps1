73091d1b143a96329cd94dc4a9990c234ee936bf
haku <haku1206@outlook.com>
2024-06-20 01:35:51 +0800

    add alias

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,3 +1,5 @@
+New-Alias -Name n -Value neovide
+
 $env:POWERSHELL_TELEMETRY_OPTOUT=1
 $env:POWERSHELL_UPDATECHECK='off'
 $env:FZF_DEFAULT_OPTS='--color=bg+:#D9D9D9,bg:#E1E1E1,border:#C8C8C8,spinner:#719899,hl:#719872,fg:#616161,header:#719872,info:#727100,pointer:#E12672,marker:#E17899,fg+:#616161,preview-bg:#D9D9D9,prompt:#0099BD,hl+:#719899'
