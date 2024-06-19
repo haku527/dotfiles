e09f30ea2c19fd66a17b62aedb03603fabd32be2
haku <haku1206@outlook.com>
2024-06-20 01:11:04 +0800

    using local variable

--- wezterm.lua
+++ wezterm.lua
@@ -3,14 +3,14 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 
-function get_appearance()
+local function get_appearance()
   if wezterm.gui then
     return wezterm.gui.get_appearance()
   end
   return 'Dark'
 end
 
-function scheme_for_appearance(appearance)
+local function scheme_for_appearance(appearance)
   if appearance:find 'Dark' then
     return 'Everforest Dark (Gogh)'
   else
@@ -30,7 +30,7 @@ config.keys = {
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   -- 只在scoop pwsh安装后应用
-  res = wezterm.glob('pwsh', os.getenv('USERPROFILE') .. '/scoop/apps')
+  local res = wezterm.glob('pwsh', os.getenv('USERPROFILE') .. '/scoop/apps')
   if #res ~= 0 then
     config.default_prog = { 'pwsh.exe' }
   end
